3720c321584636634de5808130e766d48699af98
ajoejoseph369 <ajoe.joseph@people10.com>
2024-09-30 18:39:33 +0530

    swagger dependencies added

--- src/main/java/com/p10/Inventory_Management/InventoryManagementApplication.java
+++ src/main/java/com/p10/Inventory_Management/InventoryManagementApplication.java
@@ -2,10 +2,9 @@ package com.p10.Inventory_Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+
 public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
